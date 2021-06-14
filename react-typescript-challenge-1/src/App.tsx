98c85525bf0fd34cdcda5da4c0726d51d8b91e01
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2021-06-14 12:23:54 +0100

    Added routes.

--- react-typescript-challenge-1/src/App.tsx
+++ react-typescript-challenge-1/src/App.tsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 import LoginPage from "./components/login/loginPage";
 
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <LoginPage />
-            </header>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path='/'>
+                    <div className='App'>
+                        <header className='App-header'>
+                            <LoginPage />
+                        </header>
+                    </div>
+                </Route>
+                <Route exact path='/search'>
+                    <div className='App'>
+                        <header className='App-header'></header>
+                    </div>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
