895b9d49bd2d7ce1b10f7a497c6dbc7a1534a179
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2021-10-02 19:47:21 +0100

    Test for the list of planet names.

---
+++ starwars-searcher/src/components/planetsMap/PlanetsMap.spec.tsx
@@ -0,0 +1,32 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import * as Store from "../../stores/useStore";
+import PlanetsMap from "./PlanetsMap";
+
+describe("ListOfOrganisationUsers", () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+        cleanup();
+    });
+
+    it("Component gets data from the store.", async () => {
+        jest.spyOn(Store, "useStore").mockImplementation((fn) =>
+            fn({
+                planetNames: ["Tatooine", "Mandalore"],
+                infoDict: {},
+                infoNamesArr: [],
+                api: {
+                    setPlanetsData: (data) => {},
+                    populateWithAPI: async () => {},
+                },
+            })
+        );
+
+        render(<PlanetsMap />);
+
+        const listOfPlanets = screen.getByTestId("planets-list");
+        expect(listOfPlanets.children).toHaveLength(2);
+
+        expect(screen.queryByTestId("planet-Tatooine")).toBeTruthy();
+        expect(screen.queryByTestId("planet-Mandalore")).toBeTruthy();
+    });
+});
